876496b91555da4e33e94cfe761f16941a79da8a
pbaker2018 <pbaker@hotmail.co.uk>
2018-11-23 19:35:59 +0000

    exercises q.12 only priting list if at least one student

--- typos.rb
+++ typos.rb
@@ -23,8 +23,10 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  if students.length >= 1
+    students.each do |student|
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
